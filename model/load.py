f73c9eedd11dc7a8f34e2f6d970b7ebef596ca74
Zhora Begloyan <begloyan.jora@gmail.com>
2021-06-01 12:06:59 +0400

    Load final model  Load arch and weights together

--- model/load.py
+++ model/load.py
@@ -1,13 +1,7 @@
-from tensorflow.keras.models import model_from_json
+from tensorflow.keras.models import model_from_json, load_model
 
-def init(json_path, h5_path):
-    json_file = open(json_path, 'r')
-    model_json = json_file.read()
-    json_file.close()
-    model = model_from_json(model_json)
-    model.load_weights(h5_path)
+def init(h5_path):
+    model = load_model(h5_path)
     print('Model has Loaded')
-
-    model.compile(loss='categorical_crossentropy',optimizer='adam',metrics=['accuracy'])
 
     return model
